650943546ab581e94e436e4c09958b1e3c984b4c
Miek Gieben <miek@miek.nl>
2017-08-14 07:25:03 +0100

    Fail on unknown properties

--- middleware/kubernetes/setup.go
+++ middleware/kubernetes/setup.go
@@ -72,113 +72,113 @@ func kubernetesParse(c *caddy.Controller) (*Kubernetes, error) {
 	k8s.autoPathSearch = searchFromResolvConf()
 
 	for c.Next() {
-		if c.Val() == "kubernetes" {
-			zones := c.RemainingArgs()
+		zones := c.RemainingArgs()
 
-			if len(zones) != 0 {
-				k8s.Zones = zones
-				middleware.Zones(k8s.Zones).Normalize()
-			} else {
-				k8s.Zones = make([]string, len(c.ServerBlockKeys))
-				for i := 0; i < len(c.ServerBlockKeys); i++ {
-					k8s.Zones[i] = middleware.Host(c.ServerBlockKeys[i]).Normalize()
-				}
+		if len(zones) != 0 {
+			k8s.Zones = zones
+			middleware.Zones(k8s.Zones).Normalize()
+		} else {
+			k8s.Zones = make([]string, len(c.ServerBlockKeys))
+			for i := 0; i < len(c.ServerBlockKeys); i++ {
+				k8s.Zones[i] = middleware.Host(c.ServerBlockKeys[i]).Normalize()
 			}
+		}
 
-			k8s.primaryZoneIndex = -1
-			for i, z := range k8s.Zones {
-				if strings.HasSuffix(z, "in-addr.arpa.") || strings.HasSuffix(z, "ip6.arpa.") {
+		k8s.primaryZoneIndex = -1
+		for i, z := range k8s.Zones {
+			if strings.HasSuffix(z, "in-addr.arpa.") || strings.HasSuffix(z, "ip6.arpa.") {
+				continue
+			}
+			k8s.primaryZoneIndex = i
+			break
+		}
+
+		if k8s.primaryZoneIndex == -1 {
+			return nil, errors.New("non-reverse zone name must be used")
+		}
+
+		for c.NextBlock() {
+			switch c.Val() {
+			case "pods":
+				args := c.RemainingArgs()
+				if len(args) == 1 {
+					switch args[0] {
+					case PodModeDisabled, PodModeInsecure, PodModeVerified:
+						k8s.PodMode = args[0]
+					default:
+						return nil, fmt.Errorf("wrong value for pods: %s,  must be one of: disabled, verified, insecure", args[0])
+					}
 					continue
 				}
-				k8s.primaryZoneIndex = i
-				break
-			}
-
-			if k8s.primaryZoneIndex == -1 {
-				return nil, errors.New("non-reverse zone name must be used")
-			}
-
-			for c.NextBlock() {
-				switch c.Val() {
-				case "pods":
-					args := c.RemainingArgs()
-					if len(args) == 1 {
-						switch args[0] {
-						case PodModeDisabled, PodModeInsecure, PodModeVerified:
-							k8s.PodMode = args[0]
-						default:
-							return nil, fmt.Errorf("wrong value for pods: %s,  must be one of: disabled, verified, insecure", args[0])
-						}
-						continue
-					}
-					return nil, c.ArgErr()
-				case "namespaces":
-					args := c.RemainingArgs()
-					if len(args) > 0 {
-						k8s.Namespaces = append(k8s.Namespaces, args...)
-						continue
-					}
-					return nil, c.ArgErr()
-				case "endpoint":
-					args := c.RemainingArgs()
-					if len(args) > 0 {
-						for _, endpoint := range strings.Split(args[0], ",") {
-							k8s.APIServerList = append(k8s.APIServerList, strings.TrimSpace(endpoint))
-						}
-						continue
-					}
-					return nil, c.ArgErr()
-				case "tls": // cert key cacertfile
-					args := c.RemainingArgs()
-					if len(args) == 3 {
-						k8s.APIClientCert, k8s.APIClientKey, k8s.APICertAuth = args[0], args[1], args[2]
-						continue
-					}
-					return nil, c.ArgErr()
-				case "resyncperiod":
-					args := c.RemainingArgs()
-					if len(args) > 0 {
-						rp, err := time.ParseDuration(args[0])
-						if err != nil {
-							return nil, fmt.Errorf("unable to parse resync duration value: '%v': %v", args[0], err)
-						}
-						k8s.ResyncPeriod = rp
-						continue
-					}
-					return nil, c.ArgErr()
-				case "labels":
-					args := c.RemainingArgs()
-					if len(args) > 0 {
-						labelSelectorString := strings.Join(args, " ")
-						ls, err := unversionedapi.ParseToLabelSelector(labelSelectorString)
-						if err != nil {
-							return nil, fmt.Errorf("unable to parse label selector value: '%v': %v", labelSelectorString, err)
-						}
-						k8s.LabelSelector = ls
-						continue
-					}
-					return nil, c.ArgErr()
-				case "fallthrough":
-					args := c.RemainingArgs()
-					if len(args) == 0 {
-						k8s.Fallthrough = true
-						continue
-					}
-					return nil, c.ArgErr()
-				case "upstream":
-					args := c.RemainingArgs()
-					if len(args) == 0 {
-						return nil, c.ArgErr()
-					}
-					ups, err := dnsutil.ParseHostPortOrFile(args...)
-					if err != nil {
-						return nil, err
-					}
-					k8s.Proxy = proxy.NewLookup(ups)
+				return nil, c.ArgErr()
+			case "namespaces":
+				args := c.RemainingArgs()
+				if len(args) > 0 {
+					k8s.Namespaces = append(k8s.Namespaces, args...)
+					continue
 				}
+				return nil, c.ArgErr()
+			case "endpoint":
+				args := c.RemainingArgs()
+				if len(args) > 0 {
+					for _, endpoint := range strings.Split(args[0], ",") {
+						k8s.APIServerList = append(k8s.APIServerList, strings.TrimSpace(endpoint))
+					}
+					continue
+				}
+				return nil, c.ArgErr()
+			case "tls": // cert key cacertfile
+				args := c.RemainingArgs()
+				if len(args) == 3 {
+					k8s.APIClientCert, k8s.APIClientKey, k8s.APICertAuth = args[0], args[1], args[2]
+					continue
+				}
+				return nil, c.ArgErr()
+			case "resyncperiod":
+				args := c.RemainingArgs()
+				if len(args) > 0 {
+					rp, err := time.ParseDuration(args[0])
+					if err != nil {
+						return nil, fmt.Errorf("unable to parse resync duration value: '%v': %v", args[0], err)
+					}
+					k8s.ResyncPeriod = rp
+					continue
+				}
+				return nil, c.ArgErr()
+			case "labels":
+				args := c.RemainingArgs()
+				if len(args) > 0 {
+					labelSelectorString := strings.Join(args, " ")
+					ls, err := unversionedapi.ParseToLabelSelector(labelSelectorString)
+					if err != nil {
+						return nil, fmt.Errorf("unable to parse label selector value: '%v': %v", labelSelectorString, err)
+					}
+					k8s.LabelSelector = ls
+					continue
+				}
+				return nil, c.ArgErr()
+			case "fallthrough":
+				args := c.RemainingArgs()
+				if len(args) == 0 {
+					k8s.Fallthrough = true
+					continue
+				}
+				return nil, c.ArgErr()
+			case "upstream":
+				args := c.RemainingArgs()
+				if len(args) == 0 {
+					return nil, c.ArgErr()
+				}
+				ups, err := dnsutil.ParseHostPortOrFile(args...)
+				if err != nil {
+					return nil, err
+				}
+				k8s.Proxy = proxy.NewLookup(ups)
+			default:
+				return nil, c.Errf("unknown property '%s'", c.Val())
 			}
-			return k8s, nil
 		}
+		return k8s, nil
 	}
 	return nil, errors.New("kubernetes setup called without keyword 'kubernetes' in Corefile")
 }
