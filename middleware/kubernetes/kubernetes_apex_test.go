708fe31750b2e299412fdc5dd844723f5899dee2
Miek Gieben <miek@miek.nl>
2017-08-23 10:40:07 +0100

    Test Ns request also here

--- middleware/kubernetes/kubernetes_apex_test.go
+++ middleware/kubernetes/kubernetes_apex_test.go
@@ -15,14 +15,24 @@ var kubeApexCases = [](test.Case){
 		Qname: "cluster.local.", Qtype: dns.TypeSOA,
 		Rcode: dns.RcodeSuccess,
 		Answer: []dns.RR{
-			test.SOA("cluster.local.	300	IN	SOA	ns.dns.cluster.local. hostmaster.cluster.local. 1499347823 7200 1800 86400 60"),
+			test.SOA("cluster.local.	303	IN	SOA	ns.dns.cluster.local. hostmaster.cluster.local. 1499347823 7200 1800 86400 60"),
 		},
 	},
 	{
 		Qname: "cluster.local.", Qtype: dns.TypeHINFO,
 		Rcode: dns.RcodeSuccess,
 		Ns: []dns.RR{
-			test.SOA("cluster.local.	300	IN	SOA	ns.dns.cluster.local. hostmaster.cluster.local. 1499347823 7200 1800 86400 60"),
+			test.SOA("cluster.local.	303	IN	SOA	ns.dns.cluster.local. hostmaster.cluster.local. 1499347823 7200 1800 86400 60"),
+		},
+	},
+	{
+		Qname: "cluster.local.", Qtype: dns.TypeNS,
+		Rcode: dns.RcodeSuccess,
+		Answer: []dns.RR{
+			test.NS("cluster.local. 303     IN      NS     ns.dns.cluster.local."),
+		},
+		Extra: []dns.RR{
+			test.A("ns.dns.cluster.local.   303       IN      A       127.0.0.1"),
 		},
 	},
 }
