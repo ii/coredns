2bbfa7d544e58648af05e00ec0cb19b86ba454bc
Miek Gieben <miek@miek.nl>
2018-02-06 20:43:31 +0000

    * Again do go generate and go build; still referencing the wrong forward   repo? Anyway fixed. * Use pkg/up for doing the healtchecks to cut back on unwanted queries   * Change up.Func to return an error instead of a boolean.   * Drop the string target argument as it doesn't make sense. * Add healthcheck test on failing to get an upstream answer.
    TODO(miek): double check Forward and Lookup and how they interact with
    HC, and if we correctly call close() on those

---
+++ plugin/forward/health_test.go
@@ -0,0 +1,43 @@
+package forward
+
+import (
+	"sync/atomic"
+	"testing"
+	"time"
+
+	"github.com/coredns/coredns/plugin/pkg/dnstest"
+	"github.com/coredns/coredns/plugin/test"
+	"golang.org/x/net/context"
+
+	"github.com/miekg/dns"
+)
+
+func TestHealth(t *testing.T) {
+	i := uint32(0)
+	s := dnstest.NewServer(func(w dns.ResponseWriter, r *dns.Msg) {
+		if r.Question[0].Name == "." {
+			// health check, answer
+			ret := new(dns.Msg)
+			atomic.AddUint32(&i, 1)
+			ret.SetReply(r)
+			w.WriteMsg(ret)
+		}
+		// not a health check, do a timeout
+	})
+	defer s.Close()
+
+	p := NewProxy(s.Addr)
+	f := New()
+	f.SetProxy(p)
+	defer f.Close()
+
+	req := new(dns.Msg)
+	req.SetQuestion("example.org.", dns.TypeA)
+
+	f.ServeDNS(context.TODO(), &test.ResponseWriter{}, req)
+
+	time.Sleep(2 * time.Second)
+	i1 := atomic.LoadUint32(&i)
+	println(i1)
+
+}
