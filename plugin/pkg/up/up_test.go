77315b44cb2b17cb99cb5c88e682a1fb50765f3a
Miek Gieben <miek@miek.nl>
2018-02-06 16:27:08 +0000

    fix tests

--- plugin/pkg/up/up_test.go
+++ plugin/pkg/up/up_test.go
@@ -12,7 +12,7 @@ func TestUp(t *testing.T) {
 	wg := sync.WaitGroup{}
 	hits := int32(0)
 
-	upfunc := func(s string) bool {
+	upfunc := func() bool {
 		atomic.AddInt32(&hits, 1)
 		// Sleep tiny amount so that our other pr.Do() calls hit the lock.
 		time.Sleep(3 * time.Millisecond)
@@ -25,7 +25,7 @@ func TestUp(t *testing.T) {
 
 	// These functions AddInt32 to the same hits variable, but we only want to wait when
 	// upfunc finishes, as that only calls Done() on the waitgroup.
-	upfuncNoWg := func(s string) bool { atomic.AddInt32(&hits, 1); return true }
+	upfuncNoWg := func() bool { atomic.AddInt32(&hits, 1); return true }
 	wg.Add(1)
 	pr.Do(upfunc)
 	pr.Do(upfuncNoWg)
