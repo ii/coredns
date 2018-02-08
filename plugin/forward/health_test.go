a5664a186b48ce6dc91b16a4dcdcf28003f45b96
Miek Gieben <miek@miek.nl>
2018-02-08 08:09:35 +0000

    maxfails=0 test

--- plugin/forward/health_test.go
+++ plugin/forward/health_test.go
@@ -110,5 +110,27 @@ func TestHealthFailTwice(t *testing.T) {
 }
 
 func TestHealthMaxFails(t *testing.T) {
-	// TODO
+	const expected = 0
+	i := uint32(0)
+	s := dnstest.NewServer(func(w dns.ResponseWriter, r *dns.Msg) {
+		// timeout
+	})
+	defer s.Close()
+
+	p := NewProxy(s.Addr, nil /* no TLS */)
+	f := New()
+	f.maxfails = 0
+	f.SetProxy(p)
+	defer f.Close()
+
+	req := new(dns.Msg)
+	req.SetQuestion("example.org.", dns.TypeA)
+
+	f.ServeDNS(context.TODO(), &test.ResponseWriter{}, req)
+
+	time.Sleep(1 * time.Second)
+	i1 := atomic.LoadUint32(&i)
+	if i1 != expected {
+		t.Errorf("Expected number of health checks to be %d, got %d", expected, i1)
+	}
 }
