d0dfbe22c4b8d4d94f1c38cd1bd376f1bb6a2834
Yong Tang <yong.tang.github@outlook.com>
2018-09-22 15:24:49 +0000

    Post benchmark to comments
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>

--- .benchmark.sh
+++ .benchmark.sh
@@ -3,5 +3,7 @@
 set -e +o pipefail
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" ] ; then
-   cat .benchmark.log
+    curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
+        --data-urlencode "{\"body\": \"$(cat .benchmark.log | sed "s/\"/'/g")\"}" \
+        "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
 fi
