5e6399a385d89fee6d6cb81d616ee8cd0ae363b1
Miek Gieben <miek@miek.nl>
2018-09-27 21:15:58 +0100

    Cleanup
    Signed-off-by: Miek Gieben <miek@miek.nl>

--- plugin/kubernetes/object/service.go
+++ plugin/kubernetes/object/service.go
@@ -2,9 +2,7 @@ package object
 
 import (
 	api "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/types"
 )
 
 // Service is a stripped down api.Service with only the items we need for CoreDNS.
@@ -56,42 +54,14 @@ func (s *Service) DeepCopyObject() runtime.Object {
 	return s1
 }
 
-func (s *Service) GetNamespace() string          { return s.Namespace }
-func (s *Service) SetNamespace(namespace string) {}
-func (s *Service) GetName() string               { return s.Name }
-func (s *Service) SetName(name string)           {}
+// GetNamespace implements the metav1.Object interface.
+func (s *Service) GetNamespace() string { return s.Namespace }
 
-type Object interface {
-	GetNamespace() string
-	SetNamespace(namespace string)
-	GetName() string
-	SetName(name string)
-	GetGenerateName() string
-	SetGenerateName(name string)
-	GetUID() types.UID
-	SetUID(uid types.UID)
-	GetResourceVersion() string
-	SetResourceVersion(version string)
-	GetGeneration() int64
-	SetGeneration(generation int64)
-	GetSelfLink() string
-	SetSelfLink(selfLink string)
-	GetCreationTimestamp() v1.Time
-	SetCreationTimestamp(timestamp v1.Time)
-	GetDeletionTimestamp() *v1.Time
-	SetDeletionTimestamp(timestamp *v1.Time)
-	GetDeletionGracePeriodSeconds() *int64
-	SetDeletionGracePeriodSeconds(*int64)
-	GetLabels() map[string]string
-	SetLabels(labels map[string]string)
-	GetAnnotations() map[string]string
-	SetAnnotations(annotations map[string]string)
-	GetInitializers() *v1.Initializers
-	SetInitializers(initializers *v1.Initializers)
-	GetFinalizers() []string
-	SetFinalizers(finalizers []string)
-	GetOwnerReferences() []v1.OwnerReference
-	SetOwnerReferences([]v1.OwnerReference)
-	GetClusterName() string
-	SetClusterName(clusterName string)
-}
+// SetNamespace implements the metav1.Object interface.
+func (s *Service) SetNamespace(namespace string) {}
+
+// GetName implements the metav1.Object interface.
+func (s *Service) GetName() string { return s.Name }
+
+// SetName implements the metav1.Object interface.
+func (s *Service) SetName(name string) {}
