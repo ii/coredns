1e61e3ea4e10500e833bcb149e77ddc9e54126fa
Miek Gieben <miek@miek.nl>
2018-09-27 07:57:49 +0100

    more
    Signed-off-by: Miek Gieben <miek@miek.nl>

--- plugin/kubernetes/object/object.go
+++ plugin/kubernetes/object/object.go
@@ -1,6 +1,10 @@
 package object
 
 import (
+	"fmt"
+
+	"k8s.io/apimachinery/pkg/api/meta"
+	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 )
 
@@ -12,3 +16,17 @@ func (e *Empty) GetObjectKind() schema.ObjectKind { return schema.EmptyObjectKin
 
 // ToFunc converts one empty interface to another.
 type ToFunc func(interface{}) interface{}
+
+func MetaNamespaceKeyFunc(runtime.Object) (string, error) {
+	if key, ok := obj.(ExplicitKey); ok {
+		return string(key), nil
+	}
+	meta, err := meta.Accessor(obj)
+	if err != nil {
+		return "", fmt.Errorf("object has no meta: %v", err)
+	}
+	if len(meta.GetNamespace()) > 0 {
+		return meta.GetNamespace() + "/" + meta.GetName(), nil
+	}
+	return meta.GetName(), nil
+}
