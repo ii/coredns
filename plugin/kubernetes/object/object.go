b48b172d0d6392c006a1972e2874573d02dded9d
Miek Gieben <miek@miek.nl>
2018-09-27 20:53:01 +0100

    Here goes nothing
    Signed-off-by: Miek Gieben <miek@miek.nl>

--- plugin/kubernetes/object/object.go
+++ plugin/kubernetes/object/object.go
@@ -1,11 +1,9 @@
 package object
 
 import (
-	"fmt"
-
-	"k8s.io/apimachinery/pkg/api/meta"
-	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/types"
 )
 
 // Empty is an empty struct.
@@ -17,16 +15,31 @@ func (e *Empty) GetObjectKind() schema.ObjectKind { return schema.EmptyObjectKin
 // ToFunc converts one empty interface to another.
 type ToFunc func(interface{}) interface{}
 
-func MetaNamespaceKeyFunc(runtime.Object) (string, error) {
-	if key, ok := obj.(ExplicitKey); ok {
-		return string(key), nil
-	}
-	meta, err := meta.Accessor(obj)
-	if err != nil {
-		return "", fmt.Errorf("object has no meta: %v", err)
-	}
-	if len(meta.GetNamespace()) > 0 {
-		return meta.GetNamespace() + "/" + meta.GetName(), nil
-	}
-	return meta.GetName(), nil
-}
+func (e *Empty) GetGenerateName() string                       { return "" }
+func (e *Empty) SetGenerateName(name string)                   {}
+func (e *Empty) GetUID() types.UID                             { return "" }
+func (e *Empty) SetUID(uid types.UID)                          {}
+func (e *Empty) GetResourceVersion() string                    { return "" }
+func (e *Empty) SetResourceVersion(version string)             {}
+func (e *Empty) GetGeneration() int64                          { return 0 }
+func (e *Empty) SetGeneration(generation int64)                {}
+func (e *Empty) GetSelfLink() string                           { return "" }
+func (e *Empty) SetSelfLink(selfLink string)                   {}
+func (e *Empty) GetCreationTimestamp() v1.Time                 { return v1.Time{} }
+func (e *Empty) SetCreationTimestamp(timestamp v1.Time)        {}
+func (e *Empty) GetDeletionTimestamp() *v1.Time                { return &v1.Time{} }
+func (e *Empty) SetDeletionTimestamp(timestamp *v1.Time)       {}
+func (e *Empty) GetDeletionGracePeriodSeconds() *int64         { return nil }
+func (e *Empty) SetDeletionGracePeriodSeconds(*int64)          {}
+func (e *Empty) GetLabels() map[string]string                  { return nil }
+func (e *Empty) SetLabels(labels map[string]string)            {}
+func (e *Empty) GetAnnotations() map[string]string             { return nil }
+func (e *Empty) SetAnnotations(annotations map[string]string)  {}
+func (e *Empty) GetInitializers() *v1.Initializers             { return nil }
+func (e *Empty) SetInitializers(initializers *v1.Initializers) {}
+func (e *Empty) GetFinalizers() []string                       { return nil }
+func (e *Empty) SetFinalizers(finalizers []string)             {}
+func (e *Empty) GetOwnerReferences() []v1.OwnerReference       { return nil }
+func (e *Empty) SetOwnerReferences([]v1.OwnerReference)        {}
+func (e *Empty) GetClusterName() string                        { return "" }
+func (e *Empty) SetClusterName(clusterName string)             {}
--- plugin/kubernetes/object/service.go
+++ plugin/kubernetes/object/service.go
@@ -2,7 +2,9 @@ package object
 
 import (
 	api "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 )
 
 // Service is a stripped down api.Service with only the items we need for CoreDNS.
@@ -28,7 +30,7 @@ func ToService(obj interface{}) interface{} {
 	s := &Service{
 		Name:         svc.GetName(),
 		Namespace:    svc.GetNamespace(),
-		Index:        svc.GetName() + "." + svc.GetNamespace(), // Used as index key
+		Index:        svc.GetName() + "." + svc.GetNamespace(),
 		ClusterIP:    svc.Spec.ClusterIP,
 		Type:         svc.Spec.Type,
 		ExternalName: svc.Spec.ExternalName,
@@ -42,5 +44,54 @@ var _ runtime.Object = &Service{}
 
 // DeepCopyObject implements the runtime.Object interface.
 func (s *Service) DeepCopyObject() runtime.Object {
-	return nil
+	s1 := &Service{
+		Name:         s.GetName(),
+		Namespace:    s.GetNamespace(),
+		Index:        s.GetName() + "." + s.GetNamespace(),
+		ClusterIP:    s.ClusterIP,
+		Type:         s.Type,
+		ExternalName: s.ExternalName,
+	}
+	copy(s1.Ports, s.Ports)
+	return s1
+}
+
+func (s *Service) GetNamespace() string          { return s.Namespace }
+func (s *Service) SetNamespace(namespace string) {}
+func (s *Service) GetName() string               { return s.Name }
+func (s *Service) SetName(name string)           {}
+
+type Object interface {
+	GetNamespace() string
+	SetNamespace(namespace string)
+	GetName() string
+	SetName(name string)
+	GetGenerateName() string
+	SetGenerateName(name string)
+	GetUID() types.UID
+	SetUID(uid types.UID)
+	GetResourceVersion() string
+	SetResourceVersion(version string)
+	GetGeneration() int64
+	SetGeneration(generation int64)
+	GetSelfLink() string
+	SetSelfLink(selfLink string)
+	GetCreationTimestamp() v1.Time
+	SetCreationTimestamp(timestamp v1.Time)
+	GetDeletionTimestamp() *v1.Time
+	SetDeletionTimestamp(timestamp *v1.Time)
+	GetDeletionGracePeriodSeconds() *int64
+	SetDeletionGracePeriodSeconds(*int64)
+	GetLabels() map[string]string
+	SetLabels(labels map[string]string)
+	GetAnnotations() map[string]string
+	SetAnnotations(annotations map[string]string)
+	GetInitializers() *v1.Initializers
+	SetInitializers(initializers *v1.Initializers)
+	GetFinalizers() []string
+	SetFinalizers(finalizers []string)
+	GetOwnerReferences() []v1.OwnerReference
+	SetOwnerReferences([]v1.OwnerReference)
+	GetClusterName() string
+	SetClusterName(clusterName string)
 }
