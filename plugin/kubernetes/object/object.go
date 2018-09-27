b47d6b71ad93b05cfc64e2c66229d3d1244ed23d
Miek Gieben <miek@miek.nl>
2018-09-27 21:15:58 +0100

    Cleanup
    Signed-off-by: Miek Gieben <miek@miek.nl>

--- plugin/kubernetes/object/object.go
+++ plugin/kubernetes/object/object.go
@@ -6,40 +6,95 @@ import (
 	"k8s.io/apimachinery/pkg/types"
 )
 
-// Empty is an empty struct.
-type Empty struct{}
-
-// GetObjectKind implements the ObjectKind interface as a noop.
-func (e *Empty) GetObjectKind() schema.ObjectKind { return schema.EmptyObjectKind }
-
 // ToFunc converts one empty interface to another.
 type ToFunc func(interface{}) interface{}
 
-func (e *Empty) GetGenerateName() string                       { return "" }
-func (e *Empty) SetGenerateName(name string)                   {}
-func (e *Empty) GetUID() types.UID                             { return "" }
-func (e *Empty) SetUID(uid types.UID)                          {}
-func (e *Empty) GetResourceVersion() string                    { return "" }
-func (e *Empty) SetResourceVersion(version string)             {}
-func (e *Empty) GetGeneration() int64                          { return 0 }
-func (e *Empty) SetGeneration(generation int64)                {}
-func (e *Empty) GetSelfLink() string                           { return "" }
-func (e *Empty) SetSelfLink(selfLink string)                   {}
-func (e *Empty) GetCreationTimestamp() v1.Time                 { return v1.Time{} }
-func (e *Empty) SetCreationTimestamp(timestamp v1.Time)        {}
-func (e *Empty) GetDeletionTimestamp() *v1.Time                { return &v1.Time{} }
-func (e *Empty) SetDeletionTimestamp(timestamp *v1.Time)       {}
-func (e *Empty) GetDeletionGracePeriodSeconds() *int64         { return nil }
-func (e *Empty) SetDeletionGracePeriodSeconds(*int64)          {}
-func (e *Empty) GetLabels() map[string]string                  { return nil }
-func (e *Empty) SetLabels(labels map[string]string)            {}
-func (e *Empty) GetAnnotations() map[string]string             { return nil }
-func (e *Empty) SetAnnotations(annotations map[string]string)  {}
-func (e *Empty) GetInitializers() *v1.Initializers             { return nil }
+// Empty is an empty struct.
+type Empty struct{}
+
+// GetObjectKind implementss the ObjectKind interface as a noop.
+func (e *Empty) GetObjectKind() schema.ObjectKind { return schema.EmptyObjectKind }
+
+// GetGenerateName implements the metav1.Object interface.
+func (e *Empty) GetGenerateName() string { return "" }
+
+// SetGenerateName implements the metav1.Object interface.
+func (e *Empty) SetGenerateName(name string) {}
+
+// GetUID implements the metav1.Object interface.
+func (e *Empty) GetUID() types.UID { return "" }
+
+// SetUID implements the metav1.Object interface.
+func (e *Empty) SetUID(uid types.UID) {}
+
+// GetResourceVersion implements the metav1.Object interface.
+func (e *Empty) GetResourceVersion() string { return "" }
+
+// SetResourceVersion implements the metav1.Object interface.
+func (e *Empty) SetResourceVersion(version string) {}
+
+// GetGeneration implements the metav1.Object interface.
+func (e *Empty) GetGeneration() int64 { return 0 }
+
+// SetGeneration implements the metav1.Object interface.
+func (e *Empty) SetGeneration(generation int64) {}
+
+// GetSelfLink implements the metav1.Object interface.
+func (e *Empty) GetSelfLink() string { return "" }
+
+// SetSelfLink implements the metav1.Object interface.
+func (e *Empty) SetSelfLink(selfLink string) {}
+
+// GetCreationTimestamp implements the metav1.Object interface.
+func (e *Empty) GetCreationTimestamp() v1.Time { return v1.Time{} }
+
+// SetCreationTimestamp implements the metav1.Object interface.
+func (e *Empty) SetCreationTimestamp(timestamp v1.Time) {}
+
+// GetDeletionTimestamp implements the metav1.Object interface.
+func (e *Empty) GetDeletionTimestamp() *v1.Time { return &v1.Time{} }
+
+// SetDeletionTimestamp implements the metav1.Object interface.
+func (e *Empty) SetDeletionTimestamp(timestamp *v1.Time) {}
+
+// GetDeletionGracePeriodSeconds implements the metav1.Object interface.
+func (e *Empty) GetDeletionGracePeriodSeconds() *int64 { return nil }
+
+// SetDeletionGracePeriodSeconds implements the metav1.Object interface.
+func (e *Empty) SetDeletionGracePeriodSeconds(*int64) {}
+
+// GetLabels implements the metav1.Object interface.
+func (e *Empty) GetLabels() map[string]string { return nil }
+
+// SetLabels implements the metav1.Object interface.
+func (e *Empty) SetLabels(labels map[string]string) {}
+
+// GetAnnotations implements the metav1.Object interface.
+func (e *Empty) GetAnnotations() map[string]string { return nil }
+
+// SetAnnotations implements the metav1.Object interface.
+func (e *Empty) SetAnnotations(annotations map[string]string) {}
+
+// GetInitializers implements the metav1.Object interface.
+func (e *Empty) GetInitializers() *v1.Initializers { return nil }
+
+// SetInitializers implements the metav1.Object interface.
 func (e *Empty) SetInitializers(initializers *v1.Initializers) {}
-func (e *Empty) GetFinalizers() []string                       { return nil }
-func (e *Empty) SetFinalizers(finalizers []string)             {}
-func (e *Empty) GetOwnerReferences() []v1.OwnerReference       { return nil }
-func (e *Empty) SetOwnerReferences([]v1.OwnerReference)        {}
-func (e *Empty) GetClusterName() string                        { return "" }
-func (e *Empty) SetClusterName(clusterName string)             {}
+
+// GetFinalizers implements the metav1.Object interface.
+func (e *Empty) GetFinalizers() []string { return nil }
+
+// SetFinalizers implements the metav1.Object interface.
+func (e *Empty) SetFinalizers(finalizers []string) {}
+
+// GetOwnerReferences implements the metav1.Object interface.
+func (e *Empty) GetOwnerReferences() []v1.OwnerReference { return nil }
+
+// SetOwnerReferences implements the metav1.Object interface.
+func (e *Empty) SetOwnerReferences([]v1.OwnerReference) {}
+
+// GetClusterName implements the metav1.Object interface.
+func (e *Empty) GetClusterName() string { return "" }
+
+// SetClusterName implements the metav1.Object interface.
+func (e *Empty) SetClusterName(clusterName string) {}
