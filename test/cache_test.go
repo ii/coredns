1218501b4fe73358d3524dbf623dc18ae1bc4567
Chris O'Haver <cohaver@infoblox.com>
2018-10-22 13:17:35 -0400

    Revert "plugin/cache: Set min TTL default to zero (#2199)"
    This reverts commit 4a5641c3793a750c1a0323f09678f3429b9b5649.

--- test/cache_test.go
+++ test/cache_test.go
@@ -43,28 +43,17 @@ func TestLookupCache(t *testing.T) {
 	p := proxy.NewLookup([]string{udp})
 	state := request.Request{W: &test.ResponseWriter{}, Req: new(dns.Msg)}
 
-	t.Run("Long TTL", func(t *testing.T) {
-		testCase(t, state, p, "example.org.", 2, 10)
-	})
-
-	t.Run("Short TTL", func(t *testing.T) {
-		testCase(t, state, p, "short.example.org.", 1, 1)
-	})
-
-}
-
-func testCase(t *testing.T, state request.Request, p proxy.Proxy, name string, expectAnsLen int, expectTTL uint32) {
-	resp, err := p.Lookup(state, name, dns.TypeA)
+	resp, err := p.Lookup(state, "example.org.", dns.TypeA)
 	if err != nil {
 		t.Fatal("Expected to receive reply, but didn't")
 	}
-
-	if len(resp.Answer) != expectAnsLen {
-		t.Fatalf("Expected %v RR in the answer section, got %v.", expectAnsLen, len(resp.Answer))
+	// expect answer section with A record in it
+	if len(resp.Answer) == 0 {
+		t.Fatal("Expected to at least one RR in the answer section, got none")
 	}
 
 	ttl := resp.Answer[0].Header().Ttl
-	if ttl != expectTTL {
-		t.Errorf("Expected TTL to be %d, got %d", expectTTL, ttl)
+	if ttl != 10 { // as set in the Corefile
+		t.Errorf("Expected TTL to be %d, got %d", 10, ttl)
 	}
 }
