0947499d67b09373df5710a9f1558cc2e4c17200
Miek Gieben <miek@miek.nl>
2019-07-09 16:00:54 +0100

    Give walk access to rrs
    Alternatively e.m could be exported, but just access the map of rrs
    should work as well. Another alternative would be adding a whole bunch
    of helper functions, but those need grab and return the data. Just
    having access to the rrs should be easiest for most Walks.
    
    Signed-off-by: Miek Gieben <miek@miek.nl>

--- plugin/file/tree/walk.go
+++ plugin/file/tree/walk.go
@@ -1,23 +1,25 @@
 package tree
 
+import "github.com/miekg/dns"
+
 // Walk performs fn on all values stored in the tree. A boolean is returned indicating whether the
 // Walk was interrupted by an fn returning true. If fn alters stored values' sort
 // relationships, future tree operation behaviors are undefined.
-func (t *Tree) Walk(fn func(e *Elem) bool) bool {
+func (t *Tree) Walk(fn func(e *Elem, rrs map[uint16][]dns.RR) bool) bool {
 	if t.Root == nil {
 		return false
 	}
 	return t.Root.walk(fn)
 }
 
-func (n *Node) walk(fn func(e *Elem) bool) (done bool) {
+func (n *Node) walk(fn func(e *Elem, rrs map[uint16][]dns.RR) bool) (done bool) {
 	if n.Left != nil {
 		done = n.Left.walk(fn)
 		if done {
 			return
 		}
 	}
-	done = fn(n.Elem)
+	done = fn(n.Elem, n.Elem.m)
 	if done {
 		return
 	}
